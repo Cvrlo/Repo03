263cfc982f7b6bcc884cb2d8a088354be438622d
Miran Germovšek <miran.germovsek10@gmail.com>
2022-03-30 19:35:52 +0200

    The Subtract of Two Numbers

---
+++ ex09/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+
+    var numOne;
+    var numTwo;
+    var diference = numOne - numTwo;
+
+    return diference;
+}
+
+console.log(main(15, 8));
+
+console.log(main(25, 8));
+
+console.log(main(425, 120));
+
+module.exports = main;
